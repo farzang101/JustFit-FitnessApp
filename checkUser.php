9e60328e211544c9a288a42c0bfaf5e790b0123c
Brian Montgomery <brian@montgomerycrew.net>
2020-10-01 21:05:41 -0700

    Login Page works - no encryption

--- checkUser.php
+++ checkUser.php
@@ -1,12 +1,17 @@
 <?php
 include "config.php";
 
-$uname = mysqli_real_escape_string($con,$_POST['username']);
-$password = mysqli_real_escape_string($con,$_POST['password']);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-if ($uname != "" && $password != "")
+$uname = mysqli_real_escape_string($con,$_POST['username']);
+$upassword = mysqli_real_escape_string($con,$_POST['password']);
+
+if ($uname != "" && $upassword != "")
 {
-    $sql_query = "select count(*) as cntUser from Users where Username='".$uname."' and Password='".$password."'";
+    $sql_query = ("SELECT COUNT(*) as cntUser from Users where Username='".$uname."' and Password='".$upassword."'");
+
     $result = mysqli_query($con,$sql_query);
     $row = mysqli_fetch_array($result);
 
--- config.php
+++ config.php
@@ -1,11 +1,11 @@
 <?php
 session_start();
-$host = "localhost"; /* Host name */
-$user = "root"; /* User */
-$password = ""; /* Password */
-$dbname = "tutorial"; /* Database name */
+$host         = "sql301.epizy.com";
+$dbusername     = "epiz_26795828";
+$dbpassword     = "G9LC5gD2inIh";
+$dbname       = "epiz_26795828_362GroupFDB";
 
-$con = mysqli_connect($host, $user, $password,$dbname);
+$con = mysqli_connect($host, $dbusername, $dbpassword, $dbname);
 // Check connection
 if (!$con) {
   die("Connection failed: " . mysqli_connect_error());
