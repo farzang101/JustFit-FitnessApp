ff8f2e59eba1ecbafe866984f2a3f4f6a0184758
Brian Montgomery <brian@montgomerycrew.net>
2020-10-15 19:24:33 -0700

    Updating Links

--- php/Login.php
+++ php/Login.php
@@ -1,87 +1,59 @@
-<?php
-	include "config.php";
-
-	// Check user login or not
-	if(isset($_SESSION['uname'])){
-		header('Location: UserPortal.php');
-	}
-?>
-
+  
 <!DOCTYPE html>
 <html lang="">
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<!--<link rel="stylesheet" href="../css/style.css"> -->
-		
+		<link rel="stylesheet" href="../css/Login.css">
+		<script src=p5.js></script>
+        <div class= "style1">
+			<a href="../html/About.html">About</a>
+			<a href="../html/Contact.html">Contact</a>
+			<br>
+	</div>
+        <br>
 	</head>
 	<body>
-        <!-- Header Begin -->
-        <center>
-            <div class="rowBurn">
-                <div class="column">
-                    <br>
-                        <h3>
-                            Live Workout Events
-                        </h3>
-                    </br>
-                </div>
-                <div class="column">
-                    <br>
-                        <h3>
-                            <a href="../html/Home.html">Home</a>
-                            <a href="../html/About.html">About</a>
-                            <a href="../html/Contact.html">Contact</a>
-                        </h3>
-                    </br>
-                </div>
-            </div>
-        </center>
         <!-- Header End -->
-
+        <br>
         <!-- Login Form Begin -->
+        
         <div class="container">
-
+            <h1>Login</h1>
 			<div id="div_login">
-				<h1>Login</h1>
 				<div id="message"></div>
-				<div>
+				<div class="uname">
 					<input type="text" class="textbox" id="txt_uname" name="txt_uname" placeholder="Username" />
 				</div>
-				<div>
+				<div class="pass">
 					<input type="password" class="textbox" id="txt_pwd" name="txt_pwd" placeholder="Password"/>
 				</div>
-				<div>
+				<div class="sub">
 					<input type="button" value="Submit" name="but_submit" id="but_submit" />
 				</div>
 			</div>
 		
 		</div>
         <!-- Login Form End -->
-
+<br>
         <!-- Database Login Check Script -->
-		<script src="http://code.jquery.com/jquery-3.1.1.min.js"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.0.0/core.min.js"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.0.0/x64-core.min.js"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.0.0/sha3.min.js"></script>
+        <script src="http://code.jquery.com/jquery-3.1.1.min.js"></script>
    
         <script type="text/javascript"> 
             $(document).ready(function(){
    			$("#but_submit").click(function(){
    			    var username = $("#txt_uname").val().trim();
-				var password = $("#txt_pwd").val().trim();
-				   
-				var ciphertext = CryptoJS.SHA3(password, { outputLength: 224 });
-
-   			    if( username != "" && (ciphertext.toString()) != "" ){
+   			    var password = $("#txt_pwd").val().trim();
+			
+   			    if( username != "" && password != "" ){
    			        $.ajax({
-   			            url:'checkUser.php',
+   			            url:'../php/checkUser.php',
    			            type:'post',
-   			            data:{username:username,password:(ciphertext.toString())},
+   			            data:{username:username,password:password},
    			            success:function(response){
    			                var msg = "";
    			                if(response == 1){
-                       			window.location.assign("UserPortal.php");
+                       			window.location.assign("../php/UserPortal.php");
                     		}else{
                         	msg = "Invalid username and password!";
                     		}
@@ -94,15 +66,17 @@
         </script> 
         <!-- Database Login Check Script End -->
 
-
+		<br>
         <!-- Footer Begin -->
 		<center>
+			
+			
 			<div class="rowBurn">
 				<br>
 					<h4>
+                        <hr class="hr">
 						Live Workout Events was developed by CPSC-362 Group F. All Rights Reserved.
 					</h4>
-				</br>
 			</div>
 		</center>
 		<!-- Footer End -->
