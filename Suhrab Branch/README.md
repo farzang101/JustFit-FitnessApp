a91ca6862dff9ea95008ffc3399e44b131e22ce2
syeawary <71114205+syeawary@users.noreply.github.com>
2020-10-20 18:31:14 -0700

    Add files via upload

---
+++ Suhrab Branch/README.md
@@ -0,0 +1,2 @@
+# 362GroupF
+This is the official github for group F.
---
+++ Suhrab Branch/php/Login.php
@@ -0,0 +1,98 @@
+<?php
+	include "config.php";
+
+	// Check user login or not
+	if(isset($_SESSION['uname'])){
+		header('Location: UserPortal.php');
+	}
+?>
+
+<!DOCTYPE html>
+<html lang="">
+			<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" href="../css/Login.css"> 
+		<script src=p5.js></script>
+        <div class= "style1">
+			<a href="../html/About.html">About</a>
+			<a href="../html/Contact.html">Contact</a>
+			<br>
+	</div>
+        <br>
+	</head>
+
+        <!-- Header End -->
+
+        <!-- Login Form Begin -->
+        <div class="container">
+            <h1>Login</h1>
+			<div id="div_login">
+				<div id="message"></div>
+				<div class="uname">
+					<input type="text" class="textbox" id="txt_uname" name="txt_uname" placeholder="Username" />
+				</div>
+				<div class="pass">
+					<input type="password" class="textbox" id="txt_pwd" name="txt_pwd" placeholder="Password"/>
+				</div>
+				<div class="sub">
+					<input type="button" value="Submit" name="but_submit" id="but_submit" />
+				</div>
+			</div>
+		<br>
+		</div>
+		<br>
+        <!-- Login Form End -->
+
+        <!-- Database Login Check Script -->
+		<script src="http://code.jquery.com/jquery-3.1.1.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.0.0/core.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.0.0/x64-core.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.0.0/sha3.min.js"></script>
+   
+        <script type="text/javascript"> 
+            $(document).ready(function(){
+   			$("#but_submit").click(function(){
+   			    var username = $("#txt_uname").val().trim();
+				var password = $("#txt_pwd").val().trim();
+				   
+				var ciphertext = CryptoJS.SHA3(password, { outputLength: 224 });
+
+   			    if( username != "" && (ciphertext.toString()) != "" ){
+   			        $.ajax({
+   			            url:'checkUser.php',
+   			            type:'post',
+   			            data:{username:username,password:(ciphertext.toString())},
+   			            success:function(response){
+   			                var msg = "";
+   			                if(response == 1){
+                       			window.location.assign("UserPortal.php");
+                    		}else{
+                        	msg = "Invalid username and password!";
+                    		}
+                    		$("#message").html(msg);
+   			            }
+   			        });
+   			    }
+   			});
+		});			
+        </script> 
+        <!-- Database Login Check Script End -->
+
+
+		<!-- Footer Begin -->
+		<br>
+		<center>
+		<div class="rowBurn">
+				<br>
+					<h4>
+						<br>
+						<hr>
+						<br>
+						Live Workout Events was developed by CPSC-362 Group F. All Rights Reserved.
+					</h4>
+			</div>
+		</center>
+		<!-- Footer End -->
+	</body>
+</html>
