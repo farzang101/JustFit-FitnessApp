b0c20d6fe86b99ae93802bf6c368d76b03967f66
syeawary <71114205+syeawary@users.noreply.github.com>
2020-10-22 11:40:54 -0700

    changes
    "Login" "SignUp"

--- php/SignUp.php
+++ php/SignUp.php
@@ -6,54 +6,38 @@
 		header('Location: ../php/UserPortal.php');
 	}
 ?>
-
 <!DOCTYPE html>
 <html lang="">
-	<head>
+<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<!--<link rel="stylesheet" href="../css/style.css"> -->
+		<link rel="stylesheet" href="../css/style.css"> 
+		<script src=p5.js></script>
+        <div class= "style1">
+			<a href="../html/About1.html">About</a>
+			<a href="../html/Contact1.html">Contact</a>
+			<a href=" ../php/Login.php"> Login Please</a>
+			<br>
+	</div>
+        <br>
 	</head>
 	<body>
-        <!-- Header Begin -->
-        <center>
-            <div class="rowBurn">
-                <div class="column">
-                    <br>
-                        <h3>
-                            Live Workout Events
-                        </h3>
-                    </br>
-                </div>
-                <div class="column">
-                    <br>
-                        <h3>
-                            <a href="../html/Home.html">Home</a>
-                            <a href="../html/About.html">About</a>
-                            <a href="../html/Contact.html">Contact</a>
-                        </h3>
-                    </br>
-                </div>
-            </div>
-        </center>
-        <!-- Header End -->
-
         <!-- Login Form Begin -->
         <div class="container">
 
 			<div id="div_signup">
 				<h1>Sign-Up</h1>
 				<div id="message"></div>
-				<div>
+				<div class="uname">
 					<input type="text" class="textbox" id="txt_uname" name="txt_uname" placeholder="Username" />
 				</div>
-				<div>
+				<div class="pass">
 					<input type="password" class="textbox" id="txt_pwd" name="txt_pwd" placeholder="Password"/>
                 </div>
-                <div>
+                <div class="RE-Pass">
 					<input type="password" class="textbox" id="txt_pwdre" name="txt_pwd" placeholder="Re-Type Password"/>
 				</div>
-				<div>
+				<div class="sub">
 					<input type="button" value="Submit" name="but_submit" id="but_submit" />
 				</div>
 			</div>
@@ -62,7 +46,8 @@
         <!-- Login Form End -->
 
         <!-- Database Login Check Script -->
-		<script src="http://code.jquery.com/jquery-3.1.1.min.js"></script>
+	 <!-- Database Login Check Script -->
+	 <script src="http://code.jquery.com/jquery-3.1.1.min.js"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.0.0/core.min.js"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.0.0/x64-core.min.js"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.0.0/sha3.min.js"></script>
