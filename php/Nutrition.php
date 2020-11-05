c08df8b713616dd62c576f45faf8be1183396378
syeawary <71114205+syeawary@users.noreply.github.com>
2020-11-05 13:35:53 -0800

    Nutrition.php
    Nutrition

---
+++ php/Nutrition.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html lang="">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" href="../css/Nutrition.css">
+		<script src=p5.js></script>
+		<!-- Header Begin -->
+		<!--<div class= "style2">
+			<a href="../html/Home.html">Home</a>
+			<a href="Exercise.html">Exercise</a>
+			<a href="Contact.html">Contact</a>
+			<a href="../html/About.html">About</a>
+		</div>
+	<!-- Header End -->
+         <br>
+	</head>
+	<body>
+        <div class="image">
+            <h2 style="color: darkblue">Stawberry Banana Smoothie</h2>
+        <img src="../image/Vanilla-Shake.jpg" alt="Vanilla-Shake">
+            <h3 style="color: cadetblue">Strawberry Banana Smoothie Benifits</h3>
+            <ul>
+                    <li> Rich in Fiber</li>
+                    <li> Potassium</li>
+                 </ul>
+         <h4 style="color: darkblue"> Nutrition benefits.</h4><p style="margin-right: 40%; text-align: left;"> they are rich in vitamin C, fiber, folate, and potassium.
+                They also rank higher in antioxidant power than most fruits and vegetables. Berries also contain anthocyanin, a phytochemical that helps in muscle repair.
+                    </p>
+             <h4 style="color: darkblue">Ingredients</h4>
+        <ul>
+        <li>2/3 cup Greek yogurt (plain, nonfat)</li>
+        <li>1/2 cup milk (I used 1%)</li>
+        <li>1 cup sliced strawberries</li>
+        <li>1 small banana</li>
+        <li> (Optional) adding powder protein </li>
+            </ul>
+        <h4 style="color: darkblue">Directions</h4>
+            <ul>
+                <li>Combine all ingredients in a blender and blend until smooth.</li>
+            </ul>
+            <iframe width="320" height="315"
+            src="https://www.youtube.com/embed/OBwcgKMUmqw">
+            </iframe><hr>
+            </div>
+            <!--------------------SECOND--------------------------------->
+         <div class="image2">
+              <div class="centered">
+            <h2 style="color: darkgreen"> Avocado Banana Smoothie</h2>
+        <img src="../image/avocado-banana-smoothie.jpg" alt="Vanilla-Shake">
+            <h3 style="color: saddlebrown">Avocado Banana Smoothie</h3>
+            <ul>
+                <li> Rich in Fiber</li>
+                <li> Potassium</li>
+             </ul>
+         <h4 style="color: orange"> Nutrition benefits.</h4><p style="margin-right: 40%; text-align: left;"> they are rich in vitamin C, fiber, folate, and potassium.
+                They also rank higher in antioxidant power than most fruits and vegetables. Berries also contain anthocyanin, a phytochemical that helps in muscle repair.
+                    </p>
+             <h4 style="color: darkblue">Ingredients</h4>
+        <ul>
+        <li> 1 frozen banana</li>
+        <li> 1/2 small ripe avocado  (or ¼ large)</li>
+        <li>  Handful of greens</li>
+        <li>  Cup dairy-free milk of your choice</li>
+        <li>  1/2 Cup blueberries or cut mango</li>
+        <li> (Optional)  1/2 Scoop vegan vanilla or unflavored protein powder, or to taste</li>
+            </ul>
+        <h4 style="color: darkblue">Directions</h4>
+            <ul>
+                <li>Combine all ingredients in a blender and blend until smooth.</li>
+            </ul>
+            <iframe width="320" height="315"
+            src="https://www.youtube.com/embed/TDXjck0N6IA">
+            </iframe><hr>
+             </div>
+            </div>
+        <br>
+        <!--------------------Third--------------------------------->
+        <h1 style="text-align: left; color:#FF1493; "> <u>Best Post-Workout Foods</u></h1>
+          <div class="image3">
+              <div class="centered">
+            <h2 style="color: darkgreen"> Post-Workout Salad </h2>
+        <img src="../image/post-workout-foods-salad.jpg" alt="Vanilla-Shake">
+             <h3 style="color: saddlebrown"> The salad is in rich in </h3>
+            <ul>
+                <li> Rich in Fiber</li>
+                <li> Potassium</li>
+             </ul>
+         <h4 style="color: orange"> Nutrition benefits.</h4><p style="margin-right: 40%; text-align: left;"> Salads are high in nutritional value such as improve in Muscle Performance, build stronger bones. 
+                    </p>
+             <h4 style="color: darkblue">Ingredients</h4>
+        <ul>
+        <li>    Kale and Spinach</li>
+        <li>    Almonds</li>
+        <li>    Beans</li>
+        <li>    Hard-boiled eggs</li>
+        <li>    lean grilled Chicken(Optional)</li>
+        <li>    Avacado</li>
+            </ul>
+        <h4 style="color: darkblue">Directions</h4>
+            <ul>
+                <li>Combine all ingredients in a bowl.</li>
+            </ul>
+            <iframe width="320" height="315"
+            src="https://www.youtube.com/embed/KCZJsIkEmyw">
+            </iframe><hr>
+             </div>
+            </div>
+		<!-- Header End 
+		
+		<center>
+			<div class="placeTextureBurn">
+				<br>
+					<h3>
+						My Nutrition Help
+					</h3>
+			</div>
+			<div class="row">
+				<div class="column">
+					<img src="image/BiggerInfusion.png" alt "Exercise">
+				</div>
+				<div class="column">
+					<br>
+						<h2>
+							<a href="../php/Login.php">Log in to view your nutrition information</a>
+						</h2>
+					</br>
+				</div>
+			</div> 
+		</center>
+--> <center>
+				<div class="rowBurn">
+					<br>
+						<h4>
+							<hr>
+							Live Workout Events was developed by CPSC-362 Group F. All Rights Reserved.
+						</h4>
+				
+				</div>
+			</center>
+			<!-- Footer End -->
+	</body>
+</html>
