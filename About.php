ce5e3180c897a3a8103e1c8994283ea24c7dd7dd
farzang101 <33071904+farzang101@users.noreply.github.com>
2020-11-12 14:04:50 -0800

    Add files via upload
    About.php/Contact.php/Home.php/UserPortal.php
    About.css/Contact.css/Home.css/UserPortal.css

---
+++ About.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+ <link rel="stylesheet" href="../css/About.css">
+ <link rel="stylesheet" href="../css/HeaderFooter.css">
+
+
+</head>
+<body>
+<?php include ('../html/Header.html'); ?>
+
+<div>
+  <h1 >Meet the Team</h1>
+
+         
+    </div>
+<div class="container">
+  <div class="row">
+    <div class="col-sm-4">
+      <h3>Brian Montgomery </h3>
+        <p>Brian is currently going to Cal State University Fullerton. He is the website's host, and has implemented several key bits of software including user accounts, which requires a database. He has also managed all website updates and a large amount of PHP related code. He has proven to be the de-facto team leader due to his inspiring leadership abilities. he also constantly recognizes the accomplishments of his fellow groupmates.</p>
+    </div>
+    <div class="col-sm-4">
+      <h3>Mason Godfrey</h3>
+      <p>Mason is currently going to Cal State University Fullerton, and is expected to graduate in 2020. He is the primary UI interpreter for the Live Workout Events website, and has extensive knowledge and experience surrounding audio production, video production, and image manipulation. He considers himself a Jack-Of-All-Trades. For this website, he is responsible for managing the repository and verifying that everything is up to standards. If anything, the other group members view him as a highly flexible engineer; there is not a bit of the website he has not been a part of.</p>
+    </div>
+    <div class="col-sm-4">
+      <h3>David Laguna </h3>        
+      <p>David is currently going to Cal State University Fullerton, and has made a substantial effort in order to implement a database and have it interface with the website. While this is no easy task, he continues to put in his best effort to complete his objective. He constantly works alongside Brain in order to more efficiently complete the tasks at hand. Many of the group members view him as the 'admiral' of the project, due to his constant and unwavering determination and power of will.
+</p>
+    </div>
+      <div class="col-sm-4">
+      <h3>Suhrab Yaewrey </h3>        
+      <p>Suhrab is currently going to Cal State University Fullerton. He has made it his objective to update webpages using hand-crafted code. His pages might not always be the final versions used, but they provide a basis for expansion.</p>
+    </div>
+      <div class="col-sm-4">
+      <h3>Farzan Ghaaderpanah </h3>        
+      <p>Farzan is currently going to Cal State University Fullerton. He has mainly been responsible for cleaning up html and php code. While many would be unwilling to put in the effort to do so, Farzan has made it his goal to keep the website in working order, no matter what. As such, his group members see him as a purifier, which cleans up unwanted mistakes and errors. He has worked with Mason in order to keep the website pure and untouched by bad css, html, and php code.</p>
+    </div>
+  </div>
+</div>
+
+    <?php include ('../html/Footer.html'); ?>
+
+</body>
+</html>
