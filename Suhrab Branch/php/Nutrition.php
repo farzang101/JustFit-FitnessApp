3736b6bc9c22a1b1ab9858c4783a7c6eaba21f66
syeawary <71114205+syeawary@users.noreply.github.com>
2020-11-19 19:29:08 -0800

    Nutrition

---
+++ Suhrab Branch/php/Nutrition.php
@@ -0,0 +1,238 @@
+<!DOCTYPE html>
+<html lang="">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" href="../css/Nutrition.css">
+		<script src=p5.js></script>
+	
+         <br>
+	</head>
+	<body>
+    <?php include ('../html/Header.html'); ?>	
+       
+    <body>
+          <!--------------------First--------------------------------->
+        <button onclick="myFunction()"> Smoothie</button> 
+        <div id="myDIV">
+            <div id="overflowTest">
+                <h3 style="color: red"> Quick Summary</h3>
+            <ul>
+                <li>Eating a poor diet reduces physical and mental health because eating healthy allows people to be more active. Two-thirds of people who eat fresh fruit and vegetables daily report no mental health issues, as reported by the Mental Health Foundation. Compare this to those who do have some level of mental health problems and have generally reported eating less healthy foods and more unhealthy foods. Feelings of well-being can be protected by ensuring that our diet is full of essential fats, complex carbohydrates, vitamins and minerals.</li>
+                <li>Eating a nutritious diet filled with fruits, vegetables, whole grains, dairy and protein will help maintain a healthy body weight. Janelle Hodovic, a Registered Dietician with UnityPoint Clinic - Weight Loss, says this about nutrition: “There is no one diet, no diet pill and no surgery that lets people eat whatever they want and still expect weight loss and improved health. Maintaining a healthy diet and exercise program will be what is needed to achieve those goals. Use food for its purpose: Nourishment.” Healthy Eating Plate discusses how to make the best eating choices. If eating healthy makes you think about the food you can’t have, refocus your thoughts on the foods you can have. Put a new spin on an old recipe to make it a little healthier. You don’t have to give up your favorite comfort foods; it’s all about balance. Adding a variety of fruits and vegetables to meals can help you control your weight, blood pressure and cholesterol.</li>
+                </ul></div><br>
+             <div id="overflowTest2">
+                <h3 style="color: red"> What to eat before workout</h3>
+            <ul>
+                <li>Drink Plenty of water.</li>
+                <li>Go for a pre-workout snack with carbs.</li>
+                <li>Make sure your pre-workout snack has protein.</li>
+                <li>Whole grain toast, peanut or almond butter and banana slices</li>
+                <li>Oatmeal, protein powder and blueberries</li>
+                </ul>
+                  <h3 style="color: red"> benefits of eating before workout</h3>
+            <ul>
+                <li>Studies suggest that eating or drinking carbohydrates before exercise can improve workout performance and may allow you to work out for a longer time or at a higher intensity. If you don't eat, you might feel sluggish or lightheaded when you exercise.
+                People often assume that the best fuel for exercise—especially strength training—is protein. While protein plays an important role in muscle building and repair, carbohydrates are actually what keep your body energized during long workouts.
+
+                Carbohydrates are one of your body’s best fuel sources due to the efficient way they use oxygen. In fact, they use less oxygen for every kilocalorie of energy produced than either fats or proteins, which make them an important part of your diet if you are physically active, especially if you are an endurance athlete. So, how exactly does your body use carbohydrates?</li>
+            </ul>
+                 
+            </div><br>
+        <div class="image">
+            <h2 style="color: darkblue">Stawberry Banana Smoothie</h2>
+        <img src="../image/Vanilla-Shake.jpg" alt="Vanilla-Shake">
+            <h3 style="color: cadetblue">Strawberry Banana Smoothie Benifits</h3>
+            <ul>
+                    <li> Rich in Fiber</li>
+                    <li> Potassium</li>
+                 </ul>
+         <h4 style="color: darkblue"> Nutrition benefits.</h4><p style="margin-right: 40%; text-align: left;"> they are rich in vitamin C, fiber, folate, and potassium.
+                They also rank higher in antioxidant power than most fruits and vegetables. Berries also contain anthocyanin, a phytochemical that helps in muscle repair.
+                    </p>
+             <h4 style="color: darkblue">Ingredients</h4>
+        <ul>
+        <li>2/3 cup Greek yogurt (plain, nonfat)</li>
+        <li>1/2 cup milk (I used 1%)</li>
+        <li>1 cup sliced strawberries</li>
+        <li>1 small banana</li>
+        <li> (Optional) adding powder protein </li>
+            </ul>
+        <h4 style="color: darkblue">Directions</h4>
+            <ul>
+                <li>Combine all ingredients in a blender and blend until smooth.</li>
+            </ul>
+            <iframe width="320" height="315"
+            src="https://www.youtube.com/embed/OBwcgKMUmqw">
+            </iframe><hr>
+            </div>
+        
+            <!--------------------SECOND --------------------------------->
+         <div class="image2">
+              <div class="centered">
+            <h2 style="color: darkgreen"> Avocado Banana Smoothie</h2>
+        <img src="../image/avocado-banana-smoothie.jpg" alt="Vanilla-Shake">
+            <h3 style="color: saddlebrown">Avocado Banana Smoothie</h3>
+            <ul>
+                <li> Rich in Fiber</li>
+                <li> Potassium</li>
+             </ul>
+         <h4 style="color: orange"> Nutrition benefits.</h4><p style="margin-right: 40%; text-align: left;"> they are rich in vitamin C, fiber, folate, and potassium.
+                They also rank higher in antioxidant power than most fruits and vegetables. Berries also contain anthocyanin, a phytochemical that helps in muscle repair.
+                    </p>
+             <h4 style="color: darkblue">Ingredients</h4>
+        <ul>
+        <li> 1 frozen banana</li>
+        <li> 1/2 small ripe avocado  (or ¼ large)</li>
+        <li>  Handful of greens</li>
+        <li>  Cup dairy-free milk of your choice</li>
+        <li>  1/2 Cup blueberries or cut mango</li>
+        <li> (Optional)  1/2 Scoop vegan vanilla or unflavored protein powder, or to taste</li>
+            </ul>
+        <h4 style="color: darkblue">Directions</h4>
+            <ul>
+                <li>Combine all ingredients in a blender and blend until smooth.</li>
+            </ul>
+            <iframe width="320" height="315"
+            src="https://www.youtube.com/embed/TDXjck0N6IA">
+            </iframe>
+             </div>
+            </div>
+        </div>
+        <br>
+        <!--------------------Third--------------------------------->
+     <hr>
+        <br>
+        <br>
+        <button onclick="mySalad()" 
+        style="background-color: #4CAF50; /* Green */
+        border: none;
+        color: white;
+        padding: 16px 45px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 16px;
+        margin: 4px 2px;
+        transition-duration: 0.4s;
+        cursor: pointer;"> Salad  </button>
+        <div id = " Salad" >  
+        <h1 style="text-align: left; color:#FF1493; margin-left: 40%;"> <u>Best Post-Workout Foods</u></h1>
+          <div class="image3">
+              <div class="centered">
+            <h2 style="color: darkgreen;"> Post-Workout Salad </h2>
+        <img src="../image/post-workout-foods-salad.jpg" alt="Vanilla-Shake">
+             <h3 style="color: saddlebrown;margin-left: 20%;"> The salad is in rich in </h3>
+            <ul>
+                <li> Rich in Fiber</li>
+                <li> Potassium</li>
+             </ul>
+         <h4 style="color: orange;margin-left: 20%;"> Nutrition benefits.</h4><p style="margin-left: 25%;"> Salads are high in nutritional value such as improve in Muscle Performance, build stronger bones. 
+                    </p>
+             <h4 style="color: darkblue;margin-left: 20%;">Ingredients</h4>
+        <ul>
+        <li>    Kale and Spinach</li>
+        <li>    Almonds</li>
+        <li>    Beans</li>
+        <li>    Hard-boiled eggs</li>
+        <li>    lean grilled Chicken(Optional)</li>
+        <li>    Avacado</li>
+            </ul>
+        <h4 style="color: darkblue;margin-left: 20%;">Directions</h4>
+            <ul>
+                <li>Combine all ingredients in a bowl.</li>
+            </ul>
+            <iframe width="320" height="315" 
+            src="https://www.youtube.com/embed/KCZJsIkEmyw">
+            </iframe>
+             </div>
+            </div>
+            <hr>
+        <!--------------------Fourth--------------------------------->
+          <div class="image4">
+              <div class="centered">
+        <img src="../image/Romaine-Avocado-Chicken-Salad-Recipe-3.jpg" alt="Vanilla-Shake">
+             <h3 style="color: saddlebrown"> The salad is in rich in </h3>
+            <ul>
+                <li> Rich in Fiber</li>
+                <li> Potassium</li>
+             </ul>
+         <h4 style="color: orange"> Nutrition benefits.</h4><p style="margin-left: 7%;"> Salads are high in nutritional value such as improve in Muscle Performance, build stronger bones. 
+                    </p>
+             <h4 style="color: darkblue">Ingredients</h4>
+        <ul>
+        <li>    1/2 lb (225g) chicken breast, boneless, skinless  </li>
+        <li>    1 tablespoon olive oil extra virgin</li>
+        <li>    1/2 teaspoon salt and fresh cracked black pepper, to taste</li>
+        <li>    1/4 teaspoon smoked paprika</li>
+        <li>    1 large English (or continental) cucumber, halved lengthways and sliced into 1/4-inch thick slices</li>
+        <li>    2 avocados peeled, pitted and diced</li>
+            </ul>
+        <h4 style="color: darkblue">Directions</h4>
+            <ul>
+                <li>Combine all ingredients in a bowl.</li>
+            </ul>
+            <iframe width="320" height="315"
+            src="https://www.youtube.com/embed/brdYAn6EZcU">
+            </iframe><hr>
+             </div>
+            </div>
+                <!--------------------Fifth--------------------------------->
+                <div class="image5">
+                    <div class="centered">
+              <img src="../image/avocado-toast-both.jpg" alt="Vanilla-Shake">
+                   <h3 style="color: saddlebrown; margin-left: 20%"> The salad is in rich in </h3>
+                  <ul>
+                      <li> Folate: 5% of the RDA</li>
+                      <li> Rich in Fiber</li>
+                      <li> Potassium</li>
+                      <li> Selenium: 22% of the RDA</li>
+                      <li> Vitamin B12: 9% of the RDA</li>
+                   </ul>
+               <h4 style="color: orange;margin-left: 20%"> Nutrition benefits.</h4><p > Salads are high in nutritional value such as improve in Muscle Performance, build stronger bones. 
+                          </p>
+                   <h4 style="color: darkblue;margin-left: 20%">Ingredients</h4>
+              <ul>
+              <li>    1/2 avocado, mashed</li>
+              <li>    2 slices bread, toasted</li>
+              <li>    2 eggs, poached</li>
+              <li>    4 leaves butter lettuce</li>
+              <li>    4 slices prosciutto</li>
+              <li>    1 Tbsp fresh-squeezed lemon juice</li>
+                  </ul>
+              <h4 style="color: darkblue; margin-left: 20%">Directions</h4>
+                  <ul>
+                      <li>In a pot of boiling water, add a Tbsp of vinegar and gently swirl the water in one direction. Carefully drop the eggs one at a time and poach for 2-4 minutes or until desired doneness.</li>
+                      <li>Meanwhile cut the avocados in half, remove stones and scoop out the flesh into a bowl. Mash with a fork and squeeze in fresh lemon or lime juice and salt and pepper.</li>
+                      <li>Smear toast with avocado, and top with lettuce, tomatoes, prosciutto and the poached egg. Top with sea salt and freshly cracked black pepper and serve immediately.</li>
+                  </ul>
+                  <iframe width="320" height="315"
+                  src="https://www.youtube.com/embed/brdYAn6EZcU">
+                  </iframe><hr>
+                   </div>
+                  </div>
+             </div>
+            <!-- Footer End--> 
+            <script> 
+             function myFunction() {
+        var x = document.getElementById("myDIV");
+        if (x.style.display === "none") {
+             x.style.display = "block";
+         } else {
+              x.style.display = "none";
+             }
+        }
+            </script>
+                 <script> 
+                    function mySalad() {
+               var x = document.getElementById(" Salad");
+               if (x.style.display === "none") {
+                    x.style.display = "block";
+                } else {
+                     x.style.display = "none";
+                    }
+               }
+                   </script>
+                   <?php include ('../html/Footer.html'); ?>
+	</body>
+</html>
